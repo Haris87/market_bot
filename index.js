d11db371211e0c2f4389221209ebb4ffd34fd33f
xbouchlis <x.bouchlis@atc.gr>
2021-06-23 08:59:32 +0300

    Basic structure

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
 const axios = require("axios");
 
+const TIMEINTERVAL = 5000;
+
+const wallet = {
+  USD: 2000,
+  ETH: 10,
+};
+
+const orders = [];
+
+function scanMarket() {
+  let counter = 0;
+  setInterval(async () => {
+    console.log("tick");
+    printBalance(counter);
+    const marketOrders = await getMarketOrders();
+    checkPlacedOrders(marketOrders);
+    placeNewOrders(marketOrders);
+
+    counter++;
+  }, TIMEINTERVAL);
+}
+
+function checkPlacedOrders(marketOrders) {
+  // TODO: check user's placed orders to see which ones are filled
+}
+
+function placeNewOrders(marketOrders) {
+  // TODO: place 5 bids and 5 asks
+}
+
+function printBalance(counter) {
+  // show balance every 30 seconds
+  if (counter % 6 == 0) {
+    console.log(wallet);
+  }
+}
+
+function getBestAsk() {
+  // TODO: get best ASK from market orders
+}
+
+function getBestBid() {
+  // TODO: get best BID from market orders
+}
+
 async function getMarketOrders() {
   const url = "https://api.deversifi.com/bfx/v2/book/tETHUSD/R0";
   try {
     const response = await axios.get(url);
     console.log(response.data);
+    return response.data;
   } catch (e) {
     console.log(e);
   }
 }
 
-getMarketOrders();
+scanMarket();
